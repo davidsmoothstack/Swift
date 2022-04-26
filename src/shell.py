6019eed21163f609a69c6fe151b0de56bb39bdad
David Wright <david.wright@smoothstack.com>
2022-04-26 13:52:17 -0500

    Add shell.py

---
+++ src/shell.py
@@ -0,0 +1,16 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+def ShellResult():
+    isSuccess: bool
+    statusCode: int
+
+
+def shell(command) -> ShellResult:
+    """Runs a shell command. Returns the status code"""
+    status_code = os.system(command)
+    isSuccess = status_code == 0
+
+    return ShellResult(isSuccess, status_code)
