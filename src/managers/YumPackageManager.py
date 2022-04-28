e1f2ea3c4ff5bb53b02a815d3773fb6f8ec0cfa3
David Wright <david.wright@smoothstack.com>
2022-04-27 22:49:31 -0500

    Add YumPackageManager

---
+++ src/managers/YumPackageManager.py
@@ -0,0 +1,17 @@
+from managers.BasePackageManager import BasePackageManager
+from util.shell import shell
+
+
+class YumPackageManager(BasePackageManager):
+    def install_package(self, package_name: str):
+        """Installs a package"""
+        return shell(f"sudo yum install -y {package_name}")
+
+    def is_installed(self, package_name: str):
+        """Check if the package already exists on the system"""
+        result = shell(f"yum list installed {package_name}")
+        return result.isSuccess
+
+    def upgrade_package(self, package_name: str):
+        """Upgrades the package"""
+        return shell(f"sudo yum update -y {package_name}")
--- src/util/manager_factory.py
+++ src/util/manager_factory.py
@@ -3,6 +3,7 @@ from enum import Enum
 from managers.AptPackageManager import AptPackageManager
 from managers.BasePackageManager import BasePackageManager
 from managers.SnapPackageManager import SnapPackageManager
+from managers.YumPackageManager import YumPackageManager
 
 
 def get_package_manager(package_config) -> BasePackageManager:
@@ -12,5 +13,7 @@ def get_package_manager(package_config) -> BasePackageManager:
         return AptPackageManager(package_config)
     if type == "snap":
         return SnapPackageManager(package_config)
+    if type == "yum":
+        return YumPackageManager(package_config)
     else:
         return None
